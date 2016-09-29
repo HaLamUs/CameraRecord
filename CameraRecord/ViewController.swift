66d0027bc4756dea08ba4c6f02ba9dab684886e3
Ha Lam <halam@lamha.local>
2016-09-30 00:02:02 +0700

    Record Video Camera

--- CameraRecord/ViewController.swift
+++ CameraRecord/ViewController.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import AVFoundation
+import Photos
 
 class ViewController: UIViewController {
     
@@ -15,7 +16,9 @@ class ViewController: UIViewController {
     let session = AVCaptureSession()
     var videoDeviceInput: AVCaptureDeviceInput!
     let sessionQueue = DispatchQueue(label: "CameraRecord")
-
+    var movieFileOutput: AVCaptureMovieFileOutput? = nil
+    @IBOutlet weak var recordButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         cameraView.session = session
@@ -43,82 +46,125 @@ class ViewController: UIViewController {
             strongSelf?.session.startRunning()
         }
     }
+    @IBAction func touchUpRecord(_ recordButton: UIButton) {
+        guard let movieFileOutput = self.movieFileOutput else {return}
 
+        recordButton.isEnabled = false
+        let videoPreviewLayerOrientation = cameraView.videoPreviewLayer.connection.videoOrientation
+        
+        sessionQueue.async { [weak strongSelf = self] in
+            if !movieFileOutput.isRecording {
+                let movieFileOutputConnection = self.movieFileOutput?.connection(withMediaType: AVMediaTypeVideo)
+                movieFileOutputConnection?.videoOrientation = videoPreviewLayerOrientation
+                
+                // Start recording to a temporary file.
+                let outputFileName = NSUUID().uuidString
+                let outputFilePath = (NSTemporaryDirectory() as NSString).appendingPathComponent((outputFileName as NSString).appendingPathExtension("mov")!)
+                movieFileOutput.startRecording(toOutputFileURL: URL(fileURLWithPath: outputFilePath), recordingDelegate: strongSelf)
+            }
+            else {
+                movieFileOutput.stopRecording()
+            }
+        }
+
+    }
+    
 }
 
 extension ViewController{
-     func configureSession() {
+    func configureSession() {
         session.beginConfiguration()
-        session.sessionPreset = AVCaptureSessionPresetPhoto
-        // Add video input.
-        do {
-            var defaultVideoDevice: AVCaptureDevice?
-            
-            // Choose the back dual camera if available, otherwise default to a wide angle camera.
-            if let dualCameraDevice = AVCaptureDevice.defaultDevice(withDeviceType: .builtInDuoCamera, mediaType: AVMediaTypeVideo, position: .back) {
-                defaultVideoDevice = dualCameraDevice
-            }
-            else if let backCameraDevice = AVCaptureDevice.defaultDevice(withDeviceType: .builtInWideAngleCamera, mediaType: AVMediaTypeVideo, position: .back) {
-                // If the back dual camera is not available, default to the back wide angle camera.
-                defaultVideoDevice = backCameraDevice
-            }
-            else if let frontCameraDevice = AVCaptureDevice.defaultDevice(withDeviceType: .builtInWideAngleCamera, mediaType: AVMediaTypeVideo, position: .front) {
-                // In some cases where users break their phones, the back wide angle camera is not available. In this case, we should default to the front wide angle camera.
-                defaultVideoDevice = frontCameraDevice
-            }
-            
-            let videoDeviceInput = try AVCaptureDeviceInput(device: defaultVideoDevice)
-            
-            if session.canAddInput(videoDeviceInput) {
-                session.addInput(videoDeviceInput)
-                self.videoDeviceInput = videoDeviceInput
-            }
-            else {
-                print("Could not add video device input to the session")
-                session.commitConfiguration()
-                return
-            }
+        //get camera
+        var defaultVideoDevice: AVCaptureDevice?
+        if let backCameraDevice = AVCaptureDevice.defaultDevice(withDeviceType: .builtInWideAngleCamera, mediaType: AVMediaTypeVideo, position: .back) {
+            defaultVideoDevice = backCameraDevice
         }
-        catch {
-            print("Could not create video device input: \(error)")
+        else if let frontCameraDevice = AVCaptureDevice.defaultDevice(withDeviceType: .builtInWideAngleCamera, mediaType: AVMediaTypeVideo, position: .front) {
+            defaultVideoDevice = frontCameraDevice
+        }
+        let videoDeviceInput = try? AVCaptureDeviceInput(device: defaultVideoDevice)
+        if session.canAddInput(videoDeviceInput) {
+            session.addInput(videoDeviceInput)
+            self.videoDeviceInput = videoDeviceInput
+        }
+        else {
+            print("Error can not add")
             session.commitConfiguration()
             return
         }
         
-        // Add audio input.
-        do {
-            let audioDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeAudio)
-            let audioDeviceInput = try AVCaptureDeviceInput(device: audioDevice)
-            
-            if session.canAddInput(audioDeviceInput) {
-                session.addInput(audioDeviceInput)
-            }
-            else {
-                print("Could not add audio device input to the session")
-            }
+        //get audio
+        let audioDevice = AVCaptureDevice.defaultDevice(withMediaType: AVMediaTypeAudio)
+        let audioDeviceInput = try? AVCaptureDeviceInput(device: audioDevice)
+        if session.canAddInput(audioDeviceInput) {
+            session.addInput(audioDeviceInput)
         }
-        catch {
-            print("Could not create audio device input: \(error)")
+        else {
+            print("Error can not add")
+            session.commitConfiguration()
+            return
         }
-        
-//        // Add photo output.
-//        if session.canAddOutput(photoOutput)
-//        {
-//            session.addOutput(photoOutput)
-//            
-//            photoOutput.isHighResolutionCaptureEnabled = true
-//            photoOutput.isLivePhotoCaptureEnabled = photoOutput.isLivePhotoCaptureSupported
-//            livePhotoMode = photoOutput.isLivePhotoCaptureSupported ? .on : .off
-//        }
-//        else {
-//            print("Could not add photo output to the session")
-//            setupResult = .configurationFailed
-//            session.commitConfiguration()
-//            return
-//        }
-        
-        session.commitConfiguration()
+        //trans to movie
+        let movieFileOutput = AVCaptureMovieFileOutput()
+        if self.session.canAddOutput(movieFileOutput) {
+            self.session.addOutput(movieFileOutput)
+            self.session.sessionPreset = AVCaptureSessionPresetHigh
+            if let connection = movieFileOutput.connection(withMediaType: AVMediaTypeVideo) {
+                if connection.isVideoStabilizationSupported {
+                    connection.preferredVideoStabilizationMode = .auto
+                }
+            }
+            self.session.commitConfiguration()
+            self.movieFileOutput = movieFileOutput
+        }
     }
-
+    
 }
+
+extension ViewController:AVCaptureFileOutputRecordingDelegate{
+    func capture(_ captureOutput: AVCaptureFileOutput!, didStartRecordingToOutputFileAt fileURL: URL!, fromConnections connections: [Any]!) {
+        DispatchQueue.main.async {
+            [weak strongSelf = self] in
+            strongSelf?.recordButton.isEnabled = true
+            strongSelf?.recordButton.setTitle("Stop", for: [])
+        }
+    }
+    
+    func capture(_ captureOutput: AVCaptureFileOutput!, didFinishRecordingToOutputFileAt outputFileURL: URL!, fromConnections connections: [Any]!, error: Error!) {
+        guard error == nil else {
+            return
+        }
+        PHPhotoLibrary.requestAuthorization{
+            status in
+            if status == .authorized{
+                PHPhotoLibrary.shared().performChanges({ 
+                    let options = PHAssetResourceCreationOptions()
+                    options.shouldMoveFile = true
+                    let creationRequest = PHAssetCreationRequest.forAsset()
+                    creationRequest.addResource(with: .video, fileURL: outputFileURL, options: options)
+                    }, completionHandler: { success, error in
+                         print("Error: \(error)")
+                })
+            }
+        }
+        DispatchQueue.main.async {
+            [weak strongSelf = self] in
+            strongSelf?.recordButton.isEnabled = true
+            strongSelf?.recordButton.setTitle("Record", for: [])
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
